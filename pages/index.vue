a416ee1d8cf5eac49681962611debaadb81b893b
George Xie <george.xie@activenetwork.com>
2020-05-21 13:38:42 +0800

    feat: initial nuxt

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="main">
+    <div class="header-dark">
+      <div class="logo">
+        <img src="https://miro.medium.com/max/1400/1*p0OGaNEm_F9uWaGmN517TA.jpeg" alt="image" @click="myMethod()">
+      </div>
+    </div>
+    <div class="social-nav"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    myMethod(event) {
+      scrollTo(0, 0);
+    }
+  }
+}
+</script>
